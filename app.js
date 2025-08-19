470f2f0fcb83e282391ba278358ac9b46091435c
yonatan_betito <yonatanbetito@gmail.com>
2025-08-19 19:28:39 +0300

    yonatan js Update

---
+++ app.js
@@ -0,0 +1,15 @@
+const contiener = document.getElementById("contiener");
+
+for (let index = 0; index < 100 * 30; index++) {
+  const div = document.createElement("div");
+  div.classList.add("cell");
+  if (index >= 100 * 10 && index < 100 * 11) {
+    div.classList.add("grass");
+  } else if (index >= 100 * 11 && index < 100 * 15) {
+    div.classList.add("dirt");
+  } else if (index >= 100 * 15 && index < 100 * 28) {
+    div.classList.add("stone");
+  } else if (index >= 100 * 28) {
+    div.classList.add("bedrock");
+  }
+}
