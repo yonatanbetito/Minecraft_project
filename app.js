2fa4ef553d31f0fa8a4e0c605d70617a8ae5a1f3
Dan <danhemed123@gmail.com>
2025-08-20 17:16:45 +0300

    add style backgound img for stack material

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const contiener = document.getElementById("contiener"); // contiener = grid
 const cells = document.getElementsByClassName("cell"); // all the cell in the grid
 
 // material
-const oakleaves = document.getElementsByClassName("oak-leaves"); // leaves of the tree
-const oaklog = document.getElementsByClassName("oak-log"); // root of the tree
-const grass = document.getElementsByClassName("grass");
-const dirt = document.getElementsByClassName("dirt");
-const stone = document.getElementsByClassName("stone");
+const oakleaves = document.getElementById("oak-leaves"); // leaves of the tree
+const oaklog = document.getElementById("oak-log"); // root of the tree
+const grass = document.getElementById("grass");
+const dirt = document.getElementById("dirt");
+const stone = document.getElementById("stone");
 
 // tools
-const axe = document.getElementsById("axe"); // for oak-log
-const shears = document.getElementsById("shears"); // for oak-leaves
-const shovel = document.getElementsById("shovel"); // for grass & dirt
-const pickaxe = document.getElementsById("pickaxe"); // for stone
+const axe = document.getElementById("axe"); // for oak-log
+const shears = document.getElementById("shears"); // for oak-leaves
+const shovel = document.getElementById("shovel"); // for grass & dirt
+const pickaxe = document.getElementById("pickaxe"); // for stone
 // End Element <<<
 
 let count = 0
@@ -35,27 +35,61 @@ for (let index = 0; index < 100 * 30; index++) {
   
 }
 
+const stackMaterial = {
+  oaklog: 0,
+  oakleaves: 0,
+  grass: 0,
+  dirt: 0,
+  stone: 0
+}
 
 function toolValidation(cell, tool) {
   if (tool === "axe") {
     cell.classList.remove("oak-log");
+    stackMaterial.oaklog ++;
+    oaklog.style.backgroundImage = "url(/utils/oak-leaves.webp)";
   }
   if (tool === "shears") {
     cell.classList.remove("oak-leaves");
+    stackMaterial.oakleaves++;
+    grass.style.backgroundImage = "url(/utils/oak-leaves.webp)";
   }
   if (tool === "shovel") {
     cell.classList.remove("grass");
+    stackMaterial.grass++;
+    grass.style.backgroundImage = "url(/utils/grass.webp)";
+  }
+  if (tool === "shovel") {
     cell.classList.remove("dirt");
+    stackMaterial.dirt++;
+    dirt.style.backgroundImage = "url(/utils/dirt.webp)";
   }
   if (tool === "pickaxe") {
     cell.classList.remove("stone");
+    stackMaterial.stone++;
+    stone.style.backgroundImage = "url(/utils/stone.webp)";
   }
 }
 
 function removeMaterial() {
   for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener("click", function () {
-      toolValidation(cells[i], "pickaxe");
+      toolValidation(cells[i], "shovel");
     });
   }
 } 
+
+removeMaterial();
+
+// function selecetMaterial(cell) {
+//   const 
+// }
+
+
+// function createMaterial() {
+//   for (let i = 0; i < cells.length; i++) {
+//     cells[i].addEventListener("click", (element) => {
+//       element.classList.add("")
+//     })
+//   }
+// }
